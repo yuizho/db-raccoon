69547638845ee467ab9c14813ad03098ab3f317b
yuizho <yuizho3@gmail.com>
2020-02-18 23:19:45 +0900

    modify DataSetProcessorTest
    add error test cases

--- src/test/kotlin/com/github/yuizho/dbraccoon/processor/DataSetProcessorTest.kt
+++ src/test/kotlin/com/github/yuizho/dbraccoon/processor/DataSetProcessorTest.kt
@@ -5,9 +5,12 @@ import com.github.yuizho.dbraccoon.annotation.Col
 import com.github.yuizho.dbraccoon.annotation.DataSet
 import com.github.yuizho.dbraccoon.annotation.Row
 import com.github.yuizho.dbraccoon.annotation.Table
+import com.github.yuizho.dbraccoon.exception.DbRaccoonDataSetException
+import com.github.yuizho.dbraccoon.exception.DbRaccoonException
 import com.github.yuizho.dbraccoon.operation.ColumnMetadataScanner
 import com.github.yuizho.dbraccoon.operation.Query
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.assertj.core.groups.Tuple
 import org.junit.jupiter.api.Test
 
@@ -53,6 +56,14 @@ class DataSetProcessorTest {
     }
 
     @Test
+    fun `DataSet#createInsertQueryOperator throws exception when wrong table name is passed`() {
+        val dataSet = SingleTableSingleId::class.java.getAnnotation(DataSet::class.java)
+
+        assertThatThrownBy { dataSet.createInsertQueryOperator(mapOf("wrong_table" to mapOf())) }
+                .isExactlyInstanceOf(DbRaccoonException::class.java)
+    }
+
+    @Test
     fun `DataSet#createDeleteQueryOperator creates QueryOperator`() {
         val dataSet = SingleTableSingleId::class.java.getAnnotation(DataSet::class.java)
         val actual = dataSet.createDeleteQueryOperator(
@@ -69,5 +80,34 @@ class DataSetProcessorTest {
                                 )
                         )
                 )
+    }
+
+    @Test
+    fun `DataSet#createDeleteQueryOperator throws exception when wrong table name is passed`() {
+        val dataSet = SingleTableSingleId::class.java.getAnnotation(DataSet::class.java)
+
+        assertThatThrownBy { dataSet.createDeleteQueryOperator(mapOf("wrong_table" to mapOf())) }
+                .isExactlyInstanceOf(DbRaccoonException::class.java)
+    }
+
+    @DataSet([
+        Table("test", [
+            Row([
+                Col("id", "1"),
+                Col("name", "foo")
+            ])
+        ])
+    ])
+    class NoIdColumn {}
+
+    @Test
+    fun `DataSet#createDeleteQueryOperator throws exception when the row doesn't have Id column`() {
+        val dataSet = NoIdColumn::class.java.getAnnotation(DataSet::class.java)
+
+        assertThatThrownBy {
+            dataSet.createDeleteQueryOperator(
+                    mapOf("test" to mapOf("id" to ColType.DEFAULT, "name" to ColType.DEFAULT))
+            )
+        }.isExactlyInstanceOf(DbRaccoonDataSetException::class.java)
     }
 }
