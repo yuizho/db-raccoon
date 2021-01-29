ea2b6a0a5aa876f17c8ee33e76acd46827d17bb5
yuizho <yuizho3@gmail.com>
2021-01-29 22:32:55 +0900

    modify CleanupStrategy to add kdoc comments

--- src/main/kotlin/com/github/yuizho/dbraccoon/CleanupStrategy.kt
+++ src/main/kotlin/com/github/yuizho/dbraccoon/CleanupStrategy.kt
@@ -1,13 +1,16 @@
 package com.github.yuizho.dbraccoon
 
+/**
+ * A definition of cleanup strategy
+ */
 enum class CleanupStrategy {
     /**
-     * TODO:
+     * This strategy only deletes test data row.
      */
     USED_ROWS,
 
     /**
-     * TODO:
+     * This strategy deletes all rows of the table which was inserted test data.
      */
     USED_TABLES
 }
